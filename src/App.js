a01ef3961b9622706f3d85aeb0c2fe33888caf8b
leonkenyaga <jkenyaga@gmail.com>
2023-06-06 23:49:46 +0300

    testing routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Counter from './components/Counter';
-import Login from './components/Loginpage'
-import SignUp from './components/SignUp';
+import Counter from "./components/Counter";
+import Login from "./components/Loginpage";
+import SignUp from "./components/SignUp";
 //import Postlists from "./components/postlists";
 import Post from "./components/post";
 import BasicRating from "./components/image";
 import RecipeReviewCard from "./components/Card";
-import Write from "./components/Firebasewrite"
+import Write from "./components/Firebasewrite";
+import Inputs from "./components/Inputfield";
+import Input from "./components/input";
 
 function App() {
   return (
@@ -23,6 +25,8 @@ function App() {
           <Route path="image" element={<BasicRating />} />
           <Route path="post" element={<Post />} />
           <Route path="write" element={<Write />} />
+          <Route path="inputs" element={<Inputs />} />
+          <Route path="input" element={<Input />} />
         </Route>
       </Routes>
     </BrowserRouter>
